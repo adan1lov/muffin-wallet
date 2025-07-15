4c4f69445295e87fd9209fc748d75bf6e9153c87
Alexey Danilov <aadmain@yandex.ru>
2025-07-15 19:24:07 +0300

    Fix gitignore and add gradle-wrapper.jar

--- muffin-wallet-server/src/main/java/ru/hse/muffin/wallet/server/MuffinWalletApplication.java
+++ muffin-wallet-server/src/main/java/ru/hse/muffin/wallet/server/MuffinWalletApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MuffinWalletApplication {
-    
-    public static void main(String[] args) {
-        SpringApplication.run(MuffinWalletApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MuffinWalletApplication.class, args);
+  }
 }
