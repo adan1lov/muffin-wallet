36fc0c0dc1834f392b4dad87efc1f3bafc4ca1fd
Koshkin George <goshak495@gmail.com>
2025-07-21 16:56:47 +0200

    implement repositories

--- muffin-wallet-data-api/src/main/java/ru/hse/muffin/wallet/data/api/MuffinTransactionRepository.java
+++ muffin-wallet-data-api/src/main/java/ru/hse/muffin/wallet/data/api/MuffinTransactionRepository.java
@@ -1,8 +1,15 @@
 package ru.hse.muffin.wallet.data.api;
 
+import java.util.UUID;
 import ru.hse.muffin.wallet.data.api.dto.MuffinTransaction;
 
 public interface MuffinTransactionRepository {
 
   MuffinTransaction save(MuffinTransaction transaction);
+
+  MuffinTransaction findById(UUID id);
+
+  MuffinTransaction findByFromAccount(UUID FromAccountId);
+
+  MuffinTransaction findByToAccount(UUID toAccountId);
 }
--- muffin-wallet-data-api/src/main/java/ru/hse/muffin/wallet/data/api/MuffinWalletRepository.java
+++ muffin-wallet-data-api/src/main/java/ru/hse/muffin/wallet/data/api/MuffinWalletRepository.java
@@ -1,8 +1,17 @@
 package ru.hse.muffin.wallet.data.api;
 
+import java.util.List;
+import java.util.UUID;
+
 import ru.hse.muffin.wallet.data.api.dto.MuffinWallet;
 
 public interface MuffinWalletRepository {
 
   MuffinWallet save(MuffinWallet wallet);
+
+  MuffinWallet findById(UUID id);
+
+  List<MuffinWallet> findByOwnerName(String ownerName);
+
+  MuffinWallet update(MuffinWallet wallet);
 }
